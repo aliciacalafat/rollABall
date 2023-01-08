e0bf610042a2a6228378e3a840b552130d1f8fb0
aliciacalafat <aliciacalafat@gmail.com>
2023-01-08 19:29:58 +0100

    first commit

---
+++ Assets/Scripts/ControladorCamara.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCamara : MonoBehaviour 
+{
+	public Vector3 offset;
+	private Transform target;
+
+	public float sensibilidad;
+	[Range(0,1)] public float lerpValue;
+
+	void Start()
+	{
+		target = GameObject.Find("Player").transform;
+	}
+
+	void LateUpdate()
+	{
+		transform.position = Vector3.Lerp(transform.position, target.position + offset, lerpValue);
+		offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensibilidad, Vector3.up) * offset;
+		transform.LookAt(target);
+	}
+
+}
---
+++ Assets/Scripts/PersonajeControlador.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersonajeControlador : MonoBehaviour {
+
+	public float speed;
+	public TextMeshProUGUI countText;
+	public TextMeshProUGUI winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Puntos"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "" + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "Has ganado!";
+		}
+	}
+}
